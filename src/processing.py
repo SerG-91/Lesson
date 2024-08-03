44150bbfc5e678c37dbd816408996ef65cb9948d
Sergey Titov <Ogruxa1@gimail.com>
2024-08-03 23:20:34 +0300

    add processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,14 @@
-list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-state = 'EXECUTED'
-def filter_by_state(values, filter_by1):
-    new_list = [i for i in values if i['state'] == filter_by1]
+list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+]
+
+
+def filter_by_state(values: str, filter_by: str) -> list:
+    """Функция фильтрации данных по ключу"""
+
+    new_list = [i for i in values if i["state"] == filter_by]
     return new_list
 
-print(filter_by_state(list, state))
+
+print(filter_by_state(list, "EXECUTED"))
