e51331ac0a05bff130ce52d59b75ff7ea6718f8b
Sergey Titov <Ogruxa1@gimail.com>
2024-08-28 23:31:33 +0300

    add TESTS

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,18 @@
+import logging
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(number: str) -> str:
     """Функция создания маски типа хххх хх** **** хххх для числа"""
-
+    logger.info('Преобразовываем номер карты к нужному виду')
     if len(str(number)) != 16:
+        logger.error("Был не корректно введен номер карты")
         return "не корректно введен номер"
     else:
         first_four = number[:4]
@@ -14,8 +25,9 @@ def get_mask_card_number(number: str) -> str:
 
 def get_mask_account(number: str) -> str:
     """Функция создания маски типа **хххх для числа"""
-
+    logger.info('Преобразовываем номер счета к нужному виду')
     if len(str(number)) < 20:
+        logger.error("Был не корректно введен номер счета")
         return "не корректно введен номер"
     else:
         number_str = str(number)
