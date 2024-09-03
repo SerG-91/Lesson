a1f942b49f78730e51bac42ea17842f0daf789d9
Sergey Titov <Ogruxa1@gmail.com>
2024-09-03 10:27:51 +0300

    changes utils.py and external_api.py and reader_csv_excel_file.py

---
+++ src/reader_csv_excel_file.py
@@ -0,0 +1,47 @@
+import os.path
+import logging
+import pandas as pd
+
+from config import DATA_PATH, LOGS_PATH
+
+logs_path = os.path.join(LOGS_PATH, "logi.log")
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(logs_path, mode="w")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
+def open_csv(file_name: str) -> list[dict]:
+    """Функция принимает на входе CSV файл и возвращает список словарей"""
+    try:
+        logger.info("Программа считывает файл")
+        file_excel_path = os.path.join(DATA_PATH, file_name)
+        transactions_df = pd.read_csv(file_excel_path, sep=";", decimal=",", encoding="utf-8")
+        logger.info("Программа формирует список из файла")
+        result = transactions_df.to_dict(orient="records")
+        logger.info("Программа выводит полученый результат")
+        return result
+    except Exception as error:
+        logger.error(f"При считывании файла произошла ошибка {error}.")
+
+
+def open_excel(file_name: str) -> list[dict]:
+    """Функция принимает на входе EXCEL файл и возвращает список словарей"""
+    try:
+        logger.info("Старт формирования списка")
+        file_excel_path = os.path.join(DATA_PATH, file_name)
+        excel_data = pd.read_excel(file_excel_path)
+        logger.info("Программа формирует список из файла")
+        dict_list = excel_data.to_dict(orient="records")
+        logger.info("Программа выводит полученый результат")
+        return dict_list
+    except Exception as error:
+        logger.error(f"При считывании файла произошла ошибка {error}.")
+
+
+print(open_csv("transactions.csv"))
+print(open_excel("transactions_excel.xlsx"))
