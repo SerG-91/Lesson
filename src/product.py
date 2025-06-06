0e25795e6533f48ca13f926012c5e4daeac82ef7
Sergey Titov <Ogruxa1@gimail.com>
2025-06-06 22:40:41 +0300

    add fixed

--- src/product.py
+++ src/product.py
@@ -1,4 +1,8 @@
-class Product:
+from src.BaseProduct import BaseProduct
+from src.PrintMixin import PrintMixin
+
+
+class Product(BaseProduct, PrintMixin):
     name: str
     description: str
     price: float
@@ -11,6 +15,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт"
